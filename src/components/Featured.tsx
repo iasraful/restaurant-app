73efce7c4c7204ec1ec858a04bd74417930d9013
iasraful <iasraful321@gmail.com>
2023-12-31 12:20:09 +0600

    new section completed

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,7 +1,47 @@
+import { featuredProducts } from "@/dataa";
+import Image from "next/image";
 import React from "react";
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className=" w-screen overflow-x-scroll text-red-500">
+      {/* Wrapper */}
+      <div className="w-max flex">
+        {/* Single Item */}
+        {featuredProducts.map((item) => (
+          <div
+            key={item.id}
+            className="w-screen h-[60vh] flex flex-col items-center justify-around hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+          >
+            {/* ImageContainer */}
+            <div className="relative flex-1 w-full p-6">
+              {item.img && (
+                <div className="relative flex-1 w-full h-full hover:rotate-45 transition-all duration-500">
+                  <Image
+                    className="object-contain"
+                    src={item.img}
+                    alt="Food Image"
+                    fill
+                  />
+                </div>
+              )}
+            </div>
+            {/* Text Container */}
+            <div className="flex-1 flex flex-col items-center justify-center gap-4">
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
+                {item.title}
+              </h1>
+              <p className="p-4 text-center 2xl:p-8">{item.desc}</p>
+              <span className="text-xl font-bold">${item.price}</span>
+              <button className="bg-red-500 text-white p-2 rounded-md mb-5">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
 import React from "react";
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <div className="bg-black h-screen flex flex-col md:flex-row  md:justify-between  md:bg-[url('/offerBg.png')] md:h-[70vh]">
+      {/* Text Container */}
+      <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
+        <h1 className="text-white text-5xl font-bold xl:text-6xl ">
+          Delecjous Burger & French Fry
+        </h1>
+        <p className="text-white xl:text-xl">
+          This is a demo whre the offer price will be show and there have a
+          coutndown on that time people can get buy through the offer price?
+        </p>
+        <span className="text-yellow-400 font-bold text-2xl md:text-3xl xl:text-4xl ">
+          24:55:47
+        </span>
+        <button className="bg-red-500 text-white rounded-md py-3 px-6">
+          Order Now
+        </button>
+      </div>
+      {/* Image Container  */}
+      <div className="flex-1 w-full relative md:h-full">
+        <Image
+          className="object-contain"
+          src="/offerProduct.png"
+          alt="Food Image"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
